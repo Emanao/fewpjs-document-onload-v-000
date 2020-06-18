71fa750b2eadaed2e03850698eb2a8b4dbcc06c5
Eva Navarro <emanao@me.com>
2020-06-18 00:05:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
-  document.getElementById("text").textContent = "This is really cool";
+  document.getElementById("text").textContent = "This is really cool!";
 });
 
 console.log(
